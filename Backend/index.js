3a3d54eddbb6f8caf750f03925bf459d56e07441
Piyush <piyushsainiyt868@gmail.com>
2025-03-27 12:26:25 +0530

    initial commit

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./config/db');
+const cors = require('cors');
+const User = require('./models/userModel');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
